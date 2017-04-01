1439c1e7ca02b6abfe1b5959e014fcc20820d911
Edson Melo de Souza <souzaem@outlook.com>
2017-04-01 01:02:24 -0300

    Aula 02

---
+++ aula02/src/aula02/Aula02.java
@@ -0,0 +1,20 @@
+/*
+ * Apenas para fins educacionais.
+ * Citar a fonte em caso de uso para terceiros
+ */
+package aula02;
+
+/**
+ *
+ * @author Edson Melo de Souza (prof.edson.melo@gmail.com)
+ */
+public class Aula02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Aula 02");
+    }
+    
+}
